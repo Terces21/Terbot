d89194434463c12afc1ba35b9f1bdfc549599d0a
Terces21 <123241253+Terces21@users.noreply.github.com>
2023-01-21 13:39:32 +0100

    Add files via upload

--- chess.js
+++ chess.js
@@ -1,4 +1,5 @@
 // First steps, returns the starting position of a chessboard
+let invites = [];
 const chessBoard =
   '```\n' +
   'r  n  b  q  k  b  n  r\n' +
@@ -18,9 +19,51 @@ module.exports = {
     message.channel.send(chessBoard);
   },
   startchessgame: function (message) {
-    // Get the opponent's user ID from the message
-    let opponentId = message.author.id;
-    // Send a message to confirm that the game has started
-    message.channel.send(`Game started with opponent: <@${opponentId}>`);
-    // Start the game logic here
-}}
+
+    // Get the user ID from the message to create player1
+    let Player1 = message.author.id;
+
+    // Check if someone was mentioned to create player2
+    if (message.mentions.users.size === 0) {
+      message.reply('Please mention someone to invite them to play a game.');
+    } else {
+      let Player2 = message.mentions.users.first();
+      let invite = {
+        sender: message.author.id,
+        receiver: Player2.id,
+        status: 'pending',
+        timestamp: new Date()
+      };
+      invites.push(invite);
+      // Send the invited player the invite
+      message.channel.send(`${Player2} you have been invited to a game by ${message.author}. Use !accept or !reject to accept or reject the invite.`);
+
+      // Check if the invite was accepted
+      client.on('message', message => {
+        if (message.content === '!accept') {
+          let invite = invites.find(invite => invite.receiver === message.author.id);
+          if (!invite) {
+            message.reply('You have not been invited to any game.');
+          } else {
+            invite.status = 'accepted';
+            message.channel.send(`${message.author} has accepted the game invite from ${invite.sender}. Game started between: <@${Player1}> and ${Player2}, Have fun!`);
+          }
+        }
+        if (message.content === '!reject') {
+          let invite = invites.find(invite => invite.receiver === message.author.id);
+          if (!invite) {
+            message.reply('You have not been invited to any game.');
+          } else {
+            invite.status = 'rejected';
+            message.channel.send(`${message.author} has rejected the game invite from ${invite.sender}.`);
+          }
+        }
+      });
+    }
+
+  },
+}
+
+
+
+
