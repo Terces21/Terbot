a50d790d95b07e4122d7663addca7a53c3729ccf
Terces21 <123241253+Terces21@users.noreply.github.com>
2023-01-21 13:08:38 +0100

    Add files via upload

--- chess.js
+++ chess.js
@@ -17,17 +17,10 @@ module.exports = {
   execute(message) {
     message.channel.send(chessBoard);
   },
-};
-
-// Command to initiate a new game 
-exports.startchessgame = function (message) {
-  // Get the opponent's user ID from the message
-  let opponentId = message.author.id;
-  // Send a message to confirm that the game has started
-  message.channel.send(`Game started with opponent: <@${opponentId}>`);
-  // Start the game logic here
-
-};
-
-
-
+  startchessgame: function (message) {
+    // Get the opponent's user ID from the message
+    let opponentId = message.author.id;
+    // Send a message to confirm that the game has started
+    message.channel.send(`Game started with opponent: <@${opponentId}>`);
+    // Start the game logic here
+}}
